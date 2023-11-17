2ac425a339b406f787c236b723521c542a04fd78
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2023-11-17 09:50:34 +0100

    Dizájn

--- alapok2/Form1.cs
+++ alapok2/Form1.cs
@@ -13,29 +13,41 @@ namespace alapok2
 {
     public partial class Form1 : Form
     {
-        public string[] feliratok = File.ReadAllLines("gombok.txt");
-        public int gombMagas = 30;
+        /*public string[] feliratok = File.ReadAllLines("gombok.txt");*/
+        /*public int gombMagas = 30;
         public int gombSzeles = 60;
         public int gombTavol = 20;
 
         public List<Button> gombok = new List<Button>();
-        public List<Label> cimkek = new List<Label>();
+        public List<Label> cimkek = new List<Label>();*/
 
 
         public Form1()
         {
-            int gombokSz = feliratok.Length;
+            /*int gombokSz = feliratok.Length;*/
 
-            ClientSize = new Size(400, gombokSz * (gombMagas + gombTavol) + gombTavol);
+            /*ClientSize = new Size(400, gombokSz * (gombMagas + gombTavol) + gombTavol);*/
+
+            /*this.CenterToScreen();*/
+
+            this.BackColor = Color.FromArgb(0, 0, 0);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            ClientSize = new Size(400, 300);
+            Width = 400;
+            Height = 300;
 
             //this.Width = 200;
             //this.Height = gombokSz * (gombMagas + gombTavol) + gombTavol;
 
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.BackgroundImage = Image.FromFile(@"C:\Users\Szalkai-SzaboAdam\Downloads\Szalkai-SzaboAdamFeladat20231012\alapok\bin\Debug\hatter.png");
+            /*this.BackgroundImage = Image.FromFile(@"C:\Users\Szalkai-SzaboAdam\Downloads\Szalkai-SzaboAdamFeladat20231012\alapok\bin\Debug\hatter.png")*/
+            ;
 
 
-            for (int i = 0; i < gombokSz; i++)
+            /*for (int i = 0; i < gombokSz; i++)
             {
                 Button tmp = new Button();                                             
 
@@ -48,53 +60,55 @@ namespace alapok2
 
                 gombok.Add(tmp);
                 Controls.Add(tmp);
-            }
+            }*/
 
-            
 
-            Button ki = new Button();
-            ki.Text = "Kilépés";
-            ki.Height = gombMagas;
 
-            
+             Button ki = new Button();
+             ki.Text = "Kilépés";
+             ki.Height = 30;
 
-            ki.Width = gombSzeles ;
-            ki.Location = new Point(ClientSize.Width - gombSzeles - 5, ClientSize.Height - gombMagas - 5);
-            ki.Click += new EventHandler(kiGomb);
-            Controls.Add(ki);
+            /*
 
-            for (int i = 0; i < gombokSz; i++)
+             ki.Width = gombSzeles ;
+             ki.Location = new Point(ClientSize.Width - gombSzeles - 5, ClientSize.Height - gombMagas - 5);
+             ki.Click += new EventHandler(kiGomb);
+             Controls.Add(ki);
+
+             for (int i = 0; i < gombokSz; i++)
+             {
+                 Label tmp = new Label();
+
+                 tmp.Height = gombMagas;
+                 tmp.Width = gombSzeles;
+
+                 tmp.Text = feliratok[i] + " kiválasztva!";
+                 tmp.Location = new Point(gombTavol + gombSzeles + 5, gombTavol + (gombTavol + gombMagas) * i);
+
+                 cimkek.Add(tmp);
+                 Controls.Add(tmp);
+             }
+
+             gombok[1].Select();
+             for (int i = 0; i < gombokSz; i++)
+             {
+                 if (gombok[i].CanSelect)
+                 {
+                     cimkek[i].Visible = true;
+                 } else
+                 {
+                     cimkek[i].Visible = false;
+                 }
+             }
+
+         }*/
+
+            /*static public void Main()
             {
-                Label tmp = new Label();
+                Application.Run(new Form1());
+            }*/
 
-                tmp.Height = gombMagas;
-                tmp.Width = gombSzeles;
-
-                tmp.Text = feliratok[i] + " kiválasztva!";
-                tmp.Location = new Point(gombTavol + gombSzeles + 5, gombTavol + (gombTavol + gombMagas) * i);
-                
-                cimkek.Add(tmp);
-                Controls.Add(tmp);
-            }
-
-            gombok[1].Select();
-            for (int i = 0; i < gombokSz; i++)
-            {
-                if (gombok[i].CanSelect)
-                {
-                    cimkek[i].Visible = true;
-                } else
-                {
-                    cimkek[i].Visible = false;
-                }
-            }
-
-        }
-
-        /*static public void Main()
-        {
-            Application.Run(new Form1());
-        }*/
+            /*
         private void Button_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Gomb lenyomva");
@@ -102,6 +116,7 @@ namespace alapok2
         private void kiGomb(object sender, EventArgs e)
         {
             Application.Exit();
+        }*/
         }
     }
 }
