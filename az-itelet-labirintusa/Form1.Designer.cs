ae1f0548b0cf506e919c7d10e3dde1e72e66d191
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2023-11-18 09:26:12 +0100

    Szinek beallitasa

--- az-itelet-labirintusa/Form1.Designer.cs
+++ az-itelet-labirintusa/Form1.Designer.cs
@@ -31,11 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(155, 139);
+            this.button1.Location = new System.Drawing.Point(155, 155);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(155, 169);
+            this.button2.Location = new System.Drawing.Point(155, 185);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -62,11 +64,31 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -83,6 +105,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- az-itelet-labirintusa/Form1.cs
+++ az-itelet-labirintusa/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,6 +25,10 @@ namespace az_itelet_labirintusa
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+
+
+
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -30,12 +36,29 @@ namespace az_itelet_labirintusa
             ClientSize = new Size(400, 300);
             Width = 400;
             Height = 300;
+            this.BackColor = Color.FromArgb(0, 0, 0);
 
 
-            button1.Text = "Indítás";
+            var pfc = new PrivateFontCollection();
+            pfc.AddFontFile("Bitmgothic.ttf");
+            label3.Font = new Font(pfc.Families[0], 30, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(255, 255, 255);
+            label3.Text = "Az Itelet\nLabirintusa";
+            
+
+
+            button1.Text = "Új Játék Indítás";
+
+            button2.BackColor = Color.FromArgb(21, 255, 255);
             button2.Text = "Kilépés";
 
+            label1.ForeColor = Color.FromArgb(255, 255, 255);
+            label1.Font = new Font("Arial", 7, FontStyle.Regular);
             label1.Text = "Készítette Nagy Gábor, Bodri Dávid és Szalkai-Szabó Ádám";
+
+            label2.ForeColor = Color.FromArgb(255, 255, 255);
+            label2.Font = new Font("Arial", 7, FontStyle.Regular);
+            label2.Text = "2023";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,8 +67,8 @@ namespace az_itelet_labirintusa
             /*ClientSize = new Size(1920, 1080);
             Width = 1920;
             Height = 1080;*/
-            
-           // button2.Location = new Point(10, 10);
+
+            // button2.Location = new Point(10, 10);
         }
 
         private void button2_Click(object sender, EventArgs e)
