3fd1e61197bcabe5dfa35e0cb69fb75651a2d90e
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2023-11-23 12:29:29 +0100

    design

--- az-itelet-labirintusa/Form2.Designer.cs
+++ az-itelet-labirintusa/Form2.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -37,13 +38,25 @@
             this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(996, 332);
+            this.textBox1.Location = new System.Drawing.Point(27, 208);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(585, 345);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(942, 771);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.Text = "Az ajtó egy nyugat felé haladó alagútba nyílik. Követed, míg egy elágazáshoz nem " +
+    "érsz. Ha észak felé akarsz fordulni, lapozz a 172-re. Ha nem akarsz letérni ebbő" +
+    "l a járatból. lapozz a 168-ra.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Form2
             // 
@@ -51,7 +64,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(1924, 1011);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
             this.Text = "í";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -63,5 +78,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- az-itelet-labirintusa/Form2.cs
+++ az-itelet-labirintusa/Form2.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,8 +29,14 @@ namespace az_itelet_labirintusa
             this.MaximizeBox = false;
             this.MinimizeBox = false;
 
+            var pfc = new PrivateFontCollection();
+            pfc.AddFontFile("Bitmgothic.ttf");  //Bitmgothic.ttf
+            label1.Font = new Font(pfc.Families[0], 66, FontStyle.Regular);
+            label1.Text = " Az Itelet Labirintusa";
+            //label3.ForeColor = Color.FromArgb(255, 255, 255);
+            label1.ForeColor = Color.FromArgb(0, 0, 00);
 
-
+            textBox1.Font = new Font(pfc.Families[0], 40, FontStyle.Regular);
 
             //richTextBox1.Text = "Belöknek a lenti alagútba, és rád zárják az ajtót, kizárva ezzel a nyíláson át beszűrődő természetes világosságot. Innentől kezdve kizárólag a falra rögzített fáklyáktól remélhetsz valamennyi fényt. Ahogy szemed hozzászokik a homályhoz, látod, hogy az alagút észak felé indul. Nagyot sóhajtasz a dolog igazságtalansága felett, majd elindulsz abba az irányba. Lapozz a 41-re.";
 
